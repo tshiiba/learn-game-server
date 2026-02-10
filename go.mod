4f0a1487c103a7a35c258785481533e7717a5853
tomnzcy <tomnzcy@gmail.com>
2026-02-10 22:13:42 +0900

    add: api

--- go.mod
+++ go.mod
@@ -5,19 +5,26 @@ go 1.25.6
 tool github.com/pseudomuto/protoc-gen-doc/cmd/protoc-gen-doc
 
 require (
+	google.golang.org/grpc v1.78.0
+	google.golang.org/protobuf v1.36.10
+)
+
+require (
 	github.com/Masterminds/semver v1.4.2 // indirect
 	github.com/Masterminds/sprig v2.15.0+incompatible // indirect
 	github.com/aokoli/goutils v1.0.1 // indirect
-	github.com/envoyproxy/protoc-gen-validate v0.3.0-java // indirect
+	github.com/envoyproxy/protoc-gen-validate v1.2.1 // indirect
 	github.com/gogo/protobuf v1.1.1 // indirect
-	github.com/golang/protobuf v1.5.2 // indirect
-	github.com/google/uuid v1.1.2 // indirect
+	github.com/golang/protobuf v1.5.4 // indirect
+	github.com/google/uuid v1.6.0 // indirect
 	github.com/huandu/xstrings v1.0.0 // indirect
 	github.com/imdario/mergo v0.3.4 // indirect
 	github.com/mwitkow/go-proto-validators v0.0.0-20180403085117-0950a7990007 // indirect
 	github.com/pseudomuto/protoc-gen-doc v1.5.1 // indirect
 	github.com/pseudomuto/protokit v0.2.0 // indirect
-	golang.org/x/crypto v0.0.0-20200622213623-75b288015ac9 // indirect
+	golang.org/x/crypto v0.44.0 // indirect
+	golang.org/x/net v0.47.0 // indirect
+	golang.org/x/sys v0.38.0 // indirect
+	golang.org/x/text v0.31.0 // indirect
 	google.golang.org/genproto v0.0.0-20210917145530-b395a37504d4 // indirect
-	google.golang.org/protobuf v1.27.1 // indirect
 )
--- main.go
+++ main.go
@@ -1,10 +1,79 @@
 package main
 
-import "fmt"
+import (
+	"context"
+	"fmt"
+	"log"
+	"net"
+	"os"
+	"os/signal"
+	"syscall"
+	"time"
+
+	adminv1 "github.com/tshiiba/learn-game-server/gen/go/admin/v1"
+	"google.golang.org/grpc"
+	"google.golang.org/grpc/reflection"
+)
 
 func main() {
-	added := add(3, 5)
-	fmt.Println("3 + 5 =", added)
+	addr := os.Getenv("GRPC_ADDR")
+	if addr == "" {
+		addr = ":50051"
+	}
+
+	lis, err := net.Listen("tcp", addr)
+	if err != nil {
+		log.Fatalf("failed to listen on %s: %v", addr, err)
+	}
+
+	grpcServer := grpc.NewServer()
+	adminv1.RegisterSampleServiceServer(grpcServer, &sampleService{})
+	reflection.Register(grpcServer)
+
+	errCh := make(chan error, 1)
+	go func() {
+		log.Printf("gRPC server listening on %s", addr)
+		errCh <- grpcServer.Serve(lis)
+	}()
+
+	sigCh := make(chan os.Signal, 1)
+	signal.Notify(sigCh, syscall.SIGINT, syscall.SIGTERM)
+
+	select {
+	case sig := <-sigCh:
+		log.Printf("received signal %s; shutting down", sig)
+		stopWithTimeout(grpcServer, 5*time.Second)
+	case err := <-errCh:
+		log.Fatalf("gRPC server stopped: %v", err)
+	}
+}
+
+type sampleService struct {
+	adminv1.UnimplementedSampleServiceServer
+}
+
+func (s *sampleService) Hello(ctx context.Context, req *adminv1.HelloRequest) (*adminv1.HelloResponse, error) {
+	name := req.GetName()
+	if name == "" {
+		name = "world"
+	}
+	return &adminv1.HelloResponse{Message: fmt.Sprintf("Hello, %s", name)}, nil
+}
+
+func stopWithTimeout(s *grpc.Server, timeout time.Duration) {
+	done := make(chan struct{})
+	go func() {
+		s.GracefulStop()
+		close(done)
+	}()
+
+	select {
+	case <-done:
+		return
+	case <-time.After(timeout):
+		s.Stop()
+		return
+	}
 }
 
 func add(x int, y int) int {
